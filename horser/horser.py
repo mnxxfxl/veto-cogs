c233e7aa357c3bc9271f93367f6f62680b27a452
mnxxfxl <mnxxfxl@gmail.com>
2025-10-28 16:43:13 +0700

    fix

--- horser/horser.py
+++ horser/horser.py
@@ -466,8 +466,10 @@ class Horser(commands.Cog):
         """View the horser leaderboard."""
         await ctx.send(embed=await self.get_leaderboard_embed(ctx), view=self.LeaderboardMenu(self, ctx))
 
-    async def get_sorted_top_horses(self) -> List[List]:
-        # get list of horses ordered by cash_earned desc
+    async def get_leaderboard_embed(self, ctx: commands.Context) -> discord.Embed:
+        embed = discord.Embed()
+        embed.color = discord.Color.blue()
+        embed.title = "Leaderboard"
 
         cur = self._connection.cursor()
         query = cur.execute(
@@ -478,24 +480,12 @@ class Horser(commands.Cog):
             LIMIT 25;
             """
         )
-
-        # top_horses = ((self.config.__getattr__(f'emoji_horse_{horse_color}')() + " " + horse_name, speed, power, stamina, guts, wit, cash_earned) 
-                    #   for horse_name, horse_color, speed, power, stamina, guts, wit, cash_earned in top_horses)
         
         top_horses = []
 
         for horse_name, horse_color, speed, power, stamina, guts, wit, cash_earned in query:
             emoji = await self.config.__getattr__(f'emoji_horse_{horse_color}')()
             top_horses.append([f"{emoji} {horse_name}", speed, power, stamina, guts, wit, cash_earned])
-        
-        return top_horses
-
-    async def get_leaderboard_embed(self, ctx: commands.Context) -> discord.Embed:
-        embed = discord.Embed()
-        embed.color = discord.Color.blue()
-        embed.title = "Leaderboard"
-
-        top_horses = await self.get_sorted_top_horses()
 
         if top_horses:
             top_horses = tabulate(top_horses, headers=["Horse", "Speed", "Power", "Stamina", "Guts", "Wit", "Cash Earned"], tablefmt="pretty")
